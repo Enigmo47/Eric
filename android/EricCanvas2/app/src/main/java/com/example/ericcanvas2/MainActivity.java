13cb57d7597eff840674fef4896d92fef0fa2e27
Enigmo47 <enigmo47@gmail.com>
2023-02-09 15:32:25 +0100

    Hago actividades de Canvas

--- android/EricCanvas2/app/src/main/java/com/example/ericcanvas2/MainActivity.java
+++ android/EricCanvas2/app/src/main/java/com/example/ericcanvas2/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.ericcanvas2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+
+import static android.view.View.SYSTEM_UI_FLAG_FULLSCREEN;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        MyCanvasView myCanvasView;
+        myCanvasView = new MyCanvasView(this);
+        myCanvasView.setSystemUiVisibility(SYSTEM_UI_FLAG_FULLSCREEN);
+        setContentView(myCanvasView);
     }
 }
