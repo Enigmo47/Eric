78a845a2243b8e0e6f89ba7df970cf1585c8832b
Enigmo47 <enigmo47@gmail.com>
2023-02-27 19:04:52 +0100

    cosas?

--- ADE/Workspace/Unidad6/src/Actividad_1x01.java
+++ ADE/Workspace/Unidad6/src/Actividad_1x01.java
@@ -10,7 +10,7 @@ import entrada.Teclado;
 
 public class Actividad_1x01 {
 
-	public void insertar() {
+	public static void insertar() {
 
 		Collection coleccion = null;
 		try {
@@ -18,7 +18,7 @@ public class Actividad_1x01 {
 			Database database = (Database) cl.newInstance();
 			DatabaseManager.registerDatabase(database);
 
-			String url = "xmldb:exist://localhost:8080/exist/xmlrpc/db/productos";
+			String url = "xmldb:exist://localhost:8080/exist/xmlrpc/db/ColeccionPruebas";
 			coleccion = DatabaseManager.getCollection(url, "admin", "admin");
 			if (coleccion == null) {
 				System.out.println("La colección no existe.");
@@ -72,14 +72,14 @@ public class Actividad_1x01 {
 
 	}
 
-	public void consultar() {
+	public static void consultar() {
 
 		try {
 			Class cl = Class.forName("org.exist.xmldb.DatabaseImpl");
 			Database database = (Database) cl.newInstance();
 			DatabaseManager.registerDatabase(database);
 
-			String url = "xmldb:exist://localhost:8080/exist/xmlrpc/db/productos";
+			String url = "xmldb:exist://localhost:8080/exist/xmlrpc/db/ColeccionPruebas";
 			Collection coleccion = DatabaseManager.getCollection(url, "admin", "admin");
 			if (coleccion == null) {
 				System.out.println("La colección no existe.");
@@ -117,14 +117,67 @@ public class Actividad_1x01 {
 
 	}
 
-	public void eliminar() {
+	public static void consultarPorCodigo() {
+		
 		Collection coleccion = null;
 		try {
 			Class cl = Class.forName("org.exist.xmldb.DatabaseImpl");
 			Database database = (Database) cl.newInstance();
 			DatabaseManager.registerDatabase(database);
 
-			String url = "xmldb:exist://localhost:8080/exist/xmlrpc/db/productos";
+			String url = "xmldb:exist://localhost:8080/exist/xmlrpc/db/ColeccionPruebas";
+			coleccion = DatabaseManager.getCollection(url, "admin", "admin");
+			if (coleccion == null) {
+				System.out.println("La colección no existe.");
+			}
+			else {
+				System.out.println("Conectado con éxito a la colección.");
+
+				int codigo = Teclado.leerEntero("¿Código? ");
+				String sentenciaBuscarProductoPorCodigo = 
+						"for $prod in /productos/produc " +
+						" where $prod/cod_prod = " + codigo +
+						" return $prod";
+				
+				XPathQueryService servicio = (XPathQueryService) coleccion.getService("XPathQueryService", "1.0");
+				String consulta = "for $prod in /productos/produc " + " order by $prod/denominacion " + " return $prod";
+				ResourceSet resultados = servicio.query(consulta);
+
+				int contador = 0;
+				ResourceIterator iterador = resultados.getIterator();
+				while (iterador.hasMoreResources()) {
+					Resource recurso = iterador.nextResource();
+					String producto = (String) recurso.getContent();
+					System.out.println(producto);
+					contador++;
+				}
+				System.out.println("Se han consultado " + contador + " productos.");
+				if (contador == 0) {
+					System.out.println("No se ha encontrado ningún producto.");
+				}
+
+				coleccion.close();
+			}
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println("Error de clase no encontrada: " + cnfe.getMessage());
+		} catch (InstantiationException ie) {
+			System.out.println("Error de instanciación de base de datos XML: " + ie.getMessage());
+		} catch (IllegalAccessException iae) {
+			System.out.println("Error de acceso ilegal: " + iae.getMessage());
+		} catch (XMLDBException xmldbe) {
+			System.out.println("Error de base de datos XML: " + xmldbe.getMessage());
+		}
+		
+			}
+	
+	public static void eliminar() {
+		Collection coleccion = null;
+		try {
+			Class cl = Class.forName("org.exist.xmldb.DatabaseImpl");
+			Database database = (Database) cl.newInstance();
+			DatabaseManager.registerDatabase(database);
+
+			String url = "xmldb:exist://localhost:8080/exist/xmlrpc/db/ColeccionPruebas";
 			coleccion = DatabaseManager.getCollection(url, "admin", "admin");
 			if (coleccion == null) {
 				System.out.println("La colección no existe.");
@@ -169,7 +222,7 @@ public class Actividad_1x01 {
 		}
 	}
 
-	public void actualizar() {
+	public static void actualizar() {
 		
 		Collection coleccion = null;
 		try {
@@ -249,7 +302,48 @@ public class Actividad_1x01 {
 
 	}
 
+	
+	public static String texto() {
+		return "0. Salir del programa. \n" + "1. Insertar un departamento en la base de datos\n"
+				+ "2. Eliminar un departamento, por codigo, de la base de datos.\n"
+				+ "3. Actualizar un empleado, por nombre, de la base de datos.";
+	}
+	
 	public static void main(String[] args) {
+		
+		int opcion;
+		do {
+			System.out.println(texto());
+			opcion = Teclado.leerEntero("Dime una opcion \n");
+			try {
+				switch (opcion) {
+				case 0:
+					System.out.println("Fin del programa");
+					break;
+				case 1:
+					insertar();
+					break;
+				case 2:
+					consultar();
+					break;
+				case 3:
+					actualizar();
+					break;
+				case 4:
+					eliminar();
+					break;	
+				
+
+				default:
+					System.out.println("La opcion de menu debe estar comprendida entre 0 y 3.");
+				}
+			} catch (RuntimeException e1) {
+				System.out.println(e1.getMessage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} while (opcion != 0);
+
 
 	}
 
