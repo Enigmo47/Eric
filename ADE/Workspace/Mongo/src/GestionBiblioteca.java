61521b2a952e4fcf7a1958ace8eaee3ff8b686cc
Enigmo47 <enigmo47@gmail.com>
2023-03-20 20:27:54 +0100

    cosas

--- ADE/Workspace/Mongo/src/GestionBiblioteca.java
+++ ADE/Workspace/Mongo/src/GestionBiblioteca.java
@@ -1,5 +1,4 @@
 
-
 import entrada.Teclado;
 
 public class GestionBiblioteca {
@@ -10,9 +9,8 @@ public class GestionBiblioteca {
 	 * @return Devuelve una string con el texto a imprimir
 	 */
 	public static String texto() {
-		return "0. Salir del programa. \n" 
-				+ "1. Insertar un libro en la base de datos.\n"
-				+ "2.  Consultar todos los libros de la base de datos.\n" 
+		return "0. Salir del programa. \n" + "1. Insertar un libro en la base de datos.\n"
+				+ "2. Consultar todos los libros de la base de datos.\n"
 				+ "3. Consultar un libro, por código, de la base de datos.\n"
 				+ "4. Actualizar un libro, por código, de la base de datos. \n"
 				+ "5. Eliminar un libro, por código, de la base de datos.";
@@ -30,20 +28,34 @@ public class GestionBiblioteca {
 					System.out.println("Fin del programa");
 					break;
 				case 1:
-					
+					int codigo = Teclado.leerEntero("¿Código? ");
+					String titulo = Teclado.leerCadena("¿Título? ");
+					String autor = Teclado.leerCadena("¿Autor? ");
+					int agno = Teclado.leerEntero("¿Año? ");
+					String genero = Teclado.leerCadena("¿Género? ");
+					Libro lib = new Libro(codigo, titulo, autor, agno, genero);
+					AccesoBiblioteca.insertar(lib);
 					break;
 				case 2:
-					
+					AccesoBiblioteca.consultar();
 					break;
 				case 3:
-					
+					codigo = Teclado.leerEntero("¿Código? ");
+					AccesoBiblioteca.consultarCodigo(codigo);
 					break;
 				case 4:
-					
-					break;	
+					codigo = Teclado.leerEntero("¿Código? ");
+					titulo = Teclado.leerCadena("¿Título? ");
+					autor = Teclado.leerCadena("¿Autor? ");
+					agno = Teclado.leerEntero("¿Año? ");
+					genero = Teclado.leerCadena("¿Género? ");
+					lib = new Libro(codigo, titulo, autor, agno, genero);
+					AccesoBiblioteca.actializar(lib);
+					break;
 				case 5:
-					
-					break;					
+					codigo = Teclado.leerEntero("¿Código? ");
+					AccesoBiblioteca.borrar(codigo);
+					break;
 				default:
 					System.out.println("La opcion de menu debe estar comprendida entre 0 y 5.");
 				}
@@ -54,10 +66,5 @@ public class GestionBiblioteca {
 			}
 		} while (opcion != 0);
 	}
-
-
-
-
-
 
 }
